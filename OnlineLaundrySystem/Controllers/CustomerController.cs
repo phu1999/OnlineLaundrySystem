6d1169266defd617df239f17969e75b8b1c887f3
Nghia T. Le <nghiatdotle@gmail.com>
2020-05-08 23:11:24 +0700

    Add admin template

---
+++ OnlineLaundrySystem/Controllers/CustomerController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineLaundrySystem.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult CustomersList()
+        {
+            return View();
+        }
+        public ActionResult CustomerInfo()
+        {
+            return View();
+        }
+    }
+}
